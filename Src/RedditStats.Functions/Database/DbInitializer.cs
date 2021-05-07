812ea70d8fdf93ca1f2ebd99f2deac929a64c924
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-05-06 17:04:23 -0700

    Update DbInitializer.cs

--- Src/RedditStats.Functions/Database/DbInitializer.cs
+++ Src/RedditStats.Functions/Database/DbInitializer.cs
@@ -1,9 +1,10 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace RedditStats.Functions
 {
     static class DbInitializer
     {
-        public static Task Initialize(AdvocateStatisticsDbContext advocateStatistisDbContext) => advocateStatistisDbContext.Database.EnsureCreatedAsync();
+        public static Task Initialize(AdvocateStatisticsDbContext advocateStatistisDbContext) => advocateStatistisDbContext.Database.MigrateAsync();
     }
 }
