0833133302ec615e65b82e96686527b6c2c7c3fc
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-01-24 18:01:50 -0800

    Add `global.json`

--- Src/RedditStats.Functions/Program.cs
+++ Src/RedditStats.Functions/Program.cs
@@ -15,67 +15,67 @@ namespace RedditStats.Functions;
 
 public class Program
 {
-    readonly static string _storageConnectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage") ?? string.Empty;
-    readonly static string _databaseConnectionString = Environment.GetEnvironmentVariable("DatabaseConnectionString") ?? string.Empty;
+	readonly static string _storageConnectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage") ?? string.Empty;
+	readonly static string _databaseConnectionString = Environment.GetEnvironmentVariable("DatabaseConnectionString") ?? string.Empty;
 
-    public static async Task Main(string[] args)
-    {
-        var host = CreateHostBuilder(args).Build();
+	public static async Task Main(string[] args)
+	{
+		var host = CreateHostBuilder(args).Build();
 
-        await InitializeDatabase(host);
-        await host.RunAsync();
-    }
+		await InitializeDatabase(host);
+		await host.RunAsync();
+	}
 
-    public static IHostBuilder CreateHostBuilder(string[] args)
-    {
-        return new HostBuilder()
-            .ConfigureAppConfiguration(configurationBuilder => configurationBuilder.AddCommandLine(args))
-            .ConfigureFunctionsWorkerDefaults()
-            .ConfigureServices(services =>
-            {
-                // HttpClients
-                services.AddHttpClient();
+	public static IHostBuilder CreateHostBuilder(string[] args)
+	{
+		return new HostBuilder()
+			.ConfigureAppConfiguration(configurationBuilder => configurationBuilder.AddCommandLine(args))
+			.ConfigureFunctionsWorkerDefaults()
+			.ConfigureServices(services =>
+			{
+				// HttpClients
+				services.AddHttpClient();
 
-                // Logging
-                services.AddLogging();
+				// Logging
+				services.AddLogging();
 
-                // DbContexts
-                services.AddDbContext<AdvocateStatisticsDbContext>(options => options.UseSqlServer(_databaseConnectionString));
+				// DbContexts
+				services.AddDbContext<AdvocateStatisticsDbContext>(options => options.UseSqlServer(_databaseConnectionString));
 
-                // Refit APIs
-                services.AddRefitClient<IRedditApi>()
-                    .ConfigureHttpClient(client => client.BaseAddress = new Uri(RedditApiConstants.BaseRedditApiUrl))
-                    .ConfigurePrimaryHttpMessageHandler(_ => new HttpClientHandler { AutomaticDecompression = getDecompressionMethods() })
-                    .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(3, sleepDurationProvider));
+				// Refit APIs
+				services.AddRefitClient<IRedditApi>()
+					.ConfigureHttpClient(client => client.BaseAddress = new Uri(RedditApiConstants.BaseRedditApiUrl))
+					.ConfigurePrimaryHttpMessageHandler(_ => new HttpClientHandler { AutomaticDecompression = getDecompressionMethods() })
+					.AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(3, sleepDurationProvider));
 
-                services.AddRefitClient<IAdvocateApi>()
-                    .ConfigureHttpClient(client => client.BaseAddress = new Uri(AdvocateConstants.BaseAdvocateApi))
-                    .ConfigurePrimaryHttpMessageHandler(_ => new HttpClientHandler { AutomaticDecompression = getDecompressionMethods() })
-                    .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(3, sleepDurationProvider));
+				services.AddRefitClient<IAdvocateApi>()
+					.ConfigureHttpClient(client => client.BaseAddress = new Uri(AdvocateConstants.BaseAdvocateApi))
+					.ConfigurePrimaryHttpMessageHandler(_ => new HttpClientHandler { AutomaticDecompression = getDecompressionMethods() })
+					.AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(3, sleepDurationProvider));
 
-                // Services
-                services.AddSingleton<AdvocateService>();
-                services.AddSingleton<RedditApiService>();
-            });
+				// Services
+				services.AddSingleton<AdvocateService>();
+				services.AddSingleton<RedditApiService>();
+			});
 
-        static TimeSpan sleepDurationProvider(int attemptNumber) => TimeSpan.FromSeconds(Math.Pow(2, attemptNumber));
-        static DecompressionMethods getDecompressionMethods() => DecompressionMethods.Deflate | DecompressionMethods.GZip;
-    }
+		static TimeSpan sleepDurationProvider(int attemptNumber) => TimeSpan.FromSeconds(Math.Pow(2, attemptNumber));
+		static DecompressionMethods getDecompressionMethods() => DecompressionMethods.Deflate | DecompressionMethods.GZip;
+	}
 
-    static async Task InitializeDatabase(IHost host)
-    {
-        using var scope = host.Services.CreateScope();
-        var services = scope.ServiceProvider;
+	static async Task InitializeDatabase(IHost host)
+	{
+		using var scope = host.Services.CreateScope();
+		var services = scope.ServiceProvider;
 
-        try
-        {
-            var context = services.GetRequiredService<AdvocateStatisticsDbContext>();
-            await DbInitializer.Initialize(context);
-        }
-        catch (Exception ex)
-        {
-            var logger = services.GetRequiredService<ILogger<Program>>();
-            logger.LogError(ex, "An error occurred creating the DB.");
-        }
-    }
+		try
+		{
+			var context = services.GetRequiredService<AdvocateStatisticsDbContext>();
+			await DbInitializer.Initialize(context);
+		}
+		catch (Exception ex)
+		{
+			var logger = services.GetRequiredService<ILogger<Program>>();
+			logger.LogError(ex, "An error occurred creating the DB.");
+		}
+	}
 }
